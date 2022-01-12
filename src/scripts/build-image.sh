7fdbba8b7e79ecfbd0e9244df92164ca2092a6c4
Brian Vu <emailbrian.vu@gmail.com>
2022-01-12 15:15:19 -0800

    fix: testing docker build command

--- src/scripts/build-image.sh
+++ src/scripts/build-image.sh
@@ -9,6 +9,15 @@ DOCKERFILE=$(eval echo "$PARAM_PATH")
 PROFILE_NAME=$(eval echo "$PARAM_PROFILE_NAME")
 ACCOUNT_ID=$(eval echo "\$$PARAM_ACCOUNT_ID")
 
+echo "$ACCOUNT_URL" >> test.txt
+echo "$TAG" >> test.txt
+echo "$SKIP_WHEN_TAGS_EXIST" >> test.txt
+echo "$REPO" >> test.txt
+echo "$EXTRA_BUILD_ARGS">> test.txt
+echo "$PATH" >> test.txt
+echo "$DOCKERFILE"  >> test.txt
+echo "$PROFILE_NAME" >> test.txt
+echo "$ACCOUNT_ID" >> test.txt
 # registry_id=$(echo "$ACCOUNT_URL" | sed "s;\..*;;g")
 number_of_tags_in_ecr=0
 # echo "Registry ID: $registry_id" >> test.txt
@@ -28,13 +37,13 @@ for tag in "${DOCKER_TAGS[@]}"; do
     echo "Docker tag args: $docker_tag_args" >> test.txt
 done
 if [ "$SKIP_WHEN_TAGS_EXIST" = "false" ] || [ "$SKIP_WHEN_TAGS_EXIST" = "true" -a $number_of_tags_in_ecr -lt ${#DOCKER_TAGS[@]} ]; then
-    docker build \
-
+    # docker build \
+    
         if [ -n "$EXTRA_BUILD_ARGS" ]; then
             echo "It gets to this if block" >> test.txt
-            set "$EXTRA_BUILD_ARGS"
+            set -- "$@" "$EXTRA_BUILD_ARGS"
         fi
-        -f "$PATH"/"$DOCKERFILE" \
-        "$docker_tag_args" \
-        "$PATH"
+    set -- "$@" -f "$PATH"/"$DOCKERFILE" "$docker_tag_args" "$PATH"
+    echo "$@" >> test.txt
+    docker build "$@"
 fi

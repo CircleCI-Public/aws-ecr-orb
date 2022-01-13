c00d9db9fb9b93dae03933acf15b088c61137f6f
Brian Vu <emailbrian.vu@gmail.com>
2022-01-13 11:46:00 -0800

    fix: added directory

---
+++ src/scripts/push-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ACCOUNT_URL=$(eval echo "\$$PARAM_ACCOUNT_URL")
+REPO=$(eval echo "$PARAM_REPO")
+TAG=$(eval echo "$PARAM_TAG")
+IFS="," read -ra DOCKER_TAGS <<< "$TAG"
+for tag in "${DOCKER_TAGS[@]}"; do
+    docker push "${ACCOUNT_URL}"/"$REPO":"${tag}"
+done

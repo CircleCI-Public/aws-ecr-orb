8e1ae069afed52ddad4365a62128111874873c1c
Brian Vu <emailbrian.vu@gmail.com>
2022-01-13 14:58:02 -0800

    fix: editing build command

--- src/scripts/build-image.sh
+++ src/scripts/build-image.sh
@@ -8,35 +8,30 @@ FILE_PATH=$(eval echo "$PARAM_PATH")
 DOCKERFILE=$(eval echo "$PARAM_PATH")
 PROFILE_NAME=$(eval echo "$PARAM_PROFILE_NAME")
 ACCOUNT_ID=$(eval echo "\$$PARAM_ACCOUNT_ID")
-
 number_of_tags_in_ecr=0
+# echo "Registry ID: $registry_id" >> test.txt
 docker_tag_args=""
-IIFS="," read -ra DOCKER_TAGS <<<"${TAG}"
-
+IFS="," read -ra DOCKER_TAGS <<< "${TAG}"
+# echo "Docker tags: $DOCKER_TAGS" >>test.txt
 for tag in "${DOCKER_TAGS[@]}"; do
-    if [ "$SKIP_WHEN_TAGS_EXIST" = "true" ]; then
-
-        docker_tag_exists_in_ecr=false
-
-        if [ "$docker_tag_exists_in_ecr" = "true" ]; then
-            docker pull "${ACCOUNT_URL}/${REPO}:${tag}"
-            let "number_of_tags_in_ecr+=1"
-        fi
+  if [ "${SKIP_WHEN_TAGS_EXIST}" = "true" ]; then
+    docker_tag_exists_in_ecr=$(aws ecr describe-images --profile "${PROFILE_NAME}" --registry-id "${ACCOUNT_ID}" --repository-name "${REPO}" --query "contains(imageDetails[].imageTags[], '$tag')")
+    if [ "${docker_tag_exists_in_ecr}" = "true" ]; then
+      docker pull "${ACCOUNT_URL}/${REPO}:${tag}"
+      let "number_of_tags_in_ecr+=1"
     fi
-    docker_tag_args="${docker_tag_args} -t ${ACCOUNT_URL}/${REPO}:${tag}"
-    echo "docker_tag_args $docker_tag_args"
+  fi
+  docker_tag_args="${docker_tag_args} -t ${ACCOUNT_URL}/$REPO:$tag"
+# echo "Docker tag args: $docker_tag_args" >> test.txt
 done
-
-if [ "$SKIP_WHEN_TAGS_EXIST" = "false" ] || [ "$SKIP_WHEN_TAGS_EXIST" = "true" -a $number_of_tags_in_ecr -lt ${#DOCKER_TAGS[@]} ]; then
+if [ "${SKIP_WHEN_TAGS_EXIST}" = "false" ] || [ "${SKIP_WHEN_TAGS_EXIST}" = "true" -a ${number_of_tags_in_ecr} -lt ${#DOCKER_TAGS[@]} ]; then    
     if [ -n "$EXTRA_BUILD_ARGS" ]; then
-        echo "It gets to line 35"
-        set -- "$@" "${EXTRA_BUILD_ARGS}"
-        echo "$@"
+        echo "It gets to this if block"
+        set -- "$@" "${EXTRA_BUILD_ARG}"
+    echo "$@"
     fi
-    set -- "$@" -f "$FILE_PATH"/"$DOCKERFILE" "$docker_tag_args" "$FILE_PATH"
-    echo "$@" >> test.txt
-    which docker
-
+    set -- "$@" -f "${PATH}/${DOCKERFILE} $docker_tag_args ${PATH}"
+    echo "$@"
+    set -x
     docker build "$@"
 fi
-

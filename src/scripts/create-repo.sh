9c224a4279846d4bd8ab97fde59765f98523407d
Brian Vu <emailbrian.vu@gmail.com>
2022-01-13 17:35:09 -0800

    debugging

--- src/scripts/create-repo.sh
+++ src/scripts/create-repo.sh
@@ -4,5 +4,12 @@ REGION=$(eval echo "\$${PARAM_REGION}")
 REPO=$(eval echo "${PARAM_REPO}")
 REPO_SCAN_ON_PUSH=$(eval echo "${PARAM_REPO_SCAN_ON_PUSH}")
 
+echo "${PROFILE_NAME}" >> test.txt
+echo "${REGION}" >> test.txt
+echo "${REPO}" >> test.txt
+echo "${REPO_SCAN_ON_PUSH}" >> test.txt
+echo "$REPO_SCAN_ON_PUSH" >> test.txt
+
+
 aws ecr describe-repositories --profile "${PROFILE_NAME}" --region "${REGION}" --repository-names "${REPO}" > /dev/null 2>&1 || \
-aws ecr create-repository --profile "${PROFILE_NAME}" --region "${REGION}" --repository-name "${REPO}" --image-scanning-configuration scanOnPush="$REPO_SCAN_ON_PUSH"
+aws ecr create-repository --profile "${PROFILE_NAME}" --region "${REGION}" --repository-name "${REPO}" --image-scanning-configuration scanOnPush=$REPO_SCAN_ON_PUSH

c4f0470441761af7fa1176d728d491bb9146c3d8
Brian Vu <emailbrian.vu@gmail.com>
2022-01-18 14:29:17 -0800

    fix: edited build image command

--- src/scripts/build-image.sh
+++ src/scripts/build-image.sh
@@ -8,12 +8,14 @@ FILE_PATH=$(eval echo "$PARAM_PATH")
 DOCKERFILE=$(eval echo "$PARAM_PATH")
 PROFILE_NAME=$(eval echo "$PARAM_PROFILE_NAME")
 ACCOUNT_ID=$(eval echo "\$$PARAM_ACCOUNT_ID")
+
 number_of_tags_in_ecr=0
 docker_tag_args=""
 IFS="," read -ra DOCKER_TAGS <<< "${TAG}"
 for tag in "${DOCKER_TAGS[@]}"; do
   if [ "${SKIP_WHEN_TAGS_EXIST}" = "true" ]; then
-    docker_tag_exists_in_ecr=$(aws ecr describe-images --profile "${PROFILE_NAME}" --registry-id "${ACCOUNT_ID}" --repository-name "${REPO}" --query "contains(imageDetails[].imageTags[], '${tag}')")
+      echo aws ecr describe-images --profile "${PROFILE_NAME}" --registry-id "${ACCOUNT_ID}" --repository-name "${REPO}" --query "contains(imageDetails[].imageTags[], '${tag}')" >> test.txt 
+      docker_tag_exists_in_ecr=$(aws ecr describe-images --profile "${PROFILE_NAME}" --registry-id "${ACCOUNT_ID}" --repository-name "${REPO}" --query "contains(imageDetails[].imageTags[], '${tag}')")
     if [ "${docker_tag_exists_in_ecr}" = "true" ]; then
       docker pull "${ACCOUNT_URL}/${REPO}:${tag}"
       let "number_of_tags_in_ecr+=1"
@@ -21,12 +23,16 @@ for tag in "${DOCKER_TAGS[@]}"; do
   fi
   docker_tag_args="${docker_tag_args} -t ${ACCOUNT_URL}/${REPO}:${tag}"
 done
-if [ "${SKIP_WHEN_TAGS_EXIST}" = "false" ] || [ "${SKIP_WHEN_TAGS_EXIST}" = "true" -a ${number_of_tags_in_ecr} -lt ${#DOCKER_TAGS[@]} ]; then     
-    
+if [ "${SKIP_WHEN_TAGS_EXIST}" = "false" ] || [ "${SKIP_WHEN_TAGS_EXIST}" = "true" -a ${number_of_tags_in_ecr} -lt ${#DOCKER_TAGS[@]} ]; then
+    #set -x docker build \
     if [ -n "$EXTRA_BUILD_ARGS" ]; then
-      set -- "$@" "${EXTRA_BUILD_ARGS}"
-    fi
+       set -- "$@" "${EXTRA_BUILD_ARGS}" \
+
+     fi
     set -- "$@" -f ${FILE_PATH}/${DOCKERFILE} $docker_tag_args ${FILE_PATH}
+    echo "$@" >> test.txt 
+    docker build "$@" 
+
     echo docker build "$@" >> test.txt
-    set -x docker build "$@"
+    # set -x docker build "$@"
 fi

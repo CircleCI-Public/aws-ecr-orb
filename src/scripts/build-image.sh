d1a90115810b521c170d4d91f48f5a0170a9033c
Brian Vu <emailbrian.vu@gmail.com>
2022-01-20 14:34:18 -0800

    chore: removed all references of account-url param

--- src/scripts/build-image.sh
+++ src/scripts/build-image.sh
@@ -26,6 +26,6 @@ if [ "${SKIP_WHEN_TAGS_EXIST}" = "0" ] || [ "${SKIP_WHEN_TAGS_EXIST}" = "1" -a $
     if [ -n "$EXTRA_BUILD_ARGS" ]; then
        set -- "$@" "${EXTRA_BUILD_ARGS}"
      fi
-    set -- "$@" -f "${FILE_PATH}"/"${DOCKERFILE}" ${docker_tag_args} "${FILE_PATH}"
+    set -- "$@" -f "${FILE_PATH}/${DOCKERFILE} ${docker_tag_args@Q} ${FILE_PATH}"
     docker build "$@" 
 fi

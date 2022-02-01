f7d2afc5618931d3a06a60ae09e95c84976884aa
Brian Vu <emailbrian.vu@gmail.com>
2022-02-01 14:15:33 -0800

    fix: changed region var

--- src/scripts/ecr-login.sh
+++ src/scripts/ecr-login.sh
@@ -3,4 +3,4 @@ REGION=$(eval echo "\$${PARAM_REGION}")
 PROFILE_NAME=$(eval echo "${PARAM_PROFILE_NAME}")
 ACCOUNT_ID=$(eval echo "\$${PARAM_ACCOUNT_ID}")
 ACCOUNT_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
-aws ecr get-login-password --region $"${REGION}" --profile "${PROFILE_NAME}" | docker login --username AWS --password-stdin "${ACCOUNT_URL}"
+aws ecr get-login-password --region "${REGION}" --profile "${PROFILE_NAME}" | docker login --username AWS --password-stdin "${ACCOUNT_URL}"

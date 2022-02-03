a5208e4d0f71745577545be2304b58e2a4ad6fe4
Brian Vu <emailbrian.vu@gmail.com>
2022-02-03 14:41:31 -0800

    debugging

--- src/scripts/ecr-login.sh
+++ src/scripts/ecr-login.sh
@@ -4,7 +4,7 @@ PROFILE_NAME=$(eval echo "${PARAM_PROFILE_NAME}")
 ACCOUNT_ID=$(eval echo "\$${PARAM_ACCOUNT_ID}")
 ACCOUNT_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
 echo "${ACCOUNT_URL}" > test.txt
-echo "${}" > test.txt
-echo "${ACCOUNT_URL}" > test.txt
+echo "${AWS_ACCESS_KEY_ID}" > test.txt
+echo "${AWS_SECRET_ACCESS_KEY}" > test.txt
 
 aws ecr get-login-password --region "${REGION}" --profile "${PROFILE_NAME}" | docker login --username AWS --password-stdin "${ACCOUNT_URL}"

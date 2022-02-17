ae59a862b8fd0ba3404d84501a513682e0583370
Brian Vu <emailbrian.vu@gmail.com>
2022-02-16 23:00:24 -0800

    fix: testing --profile flag

--- src/scripts/ecr-login.sh
+++ src/scripts/ecr-login.sh
@@ -4,9 +4,13 @@ PROFILE_NAME=$(eval echo "${PARAM_PROFILE_NAME}")
 ACCOUNT_ID=$(eval echo "\$${PARAM_ACCOUNT_ID}")
 ACCOUNT_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
 
+if [ -n "${PROFILE_NAME}" ]; then
+    set -- "$@" --profile "${PROFILE_NAME}"
+fi
+
 if [ -f ~/.docker/config.json ]; then
-    echo "Credential helper installed"
+    echo "Credential helper is already installed"
 else
-    aws ecr get-login-password --region "${REGION}" | docker login --username AWS --password-stdin "${ACCOUNT_URL}"
+    aws ecr get-login-password --region "${REGION}" "$@" | docker login --username AWS --password-stdin "${ACCOUNT_URL}"
     # aws ecr-public get-login-password --region us-east-1 --profile | docker login --username AWS --password-stdin public.ecr.aws
 fi

2fa4be100478cc364fd382ffdb4255e3c02ec888
Brian Vu <emailbrian.vu@gmail.com>
2022-03-01 12:25:35 -0800

    fix: testing expansion

--- src/scripts/create-repo.sh
+++ src/scripts/create-repo.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-# PROFILE_NAME=$(eval echo "${PARAM_PROFILE_NAME}")
-# REGION=$(eval echo "${PARAM_REGION}")
-# REPO=$(eval echo "${PARAM_REPO}")
-# REPO_SCAN_ON_PUSH=$(eval echo "${PARAM_REPO_SCAN_ON_PUSH}")
-# PUBLIC_REGISTRY=$(eval echo "${PARAM_PUBLIC_REGISTRY}")
+REGION=$(eval echo "${PARAM_REGION}")
 
-PROFILE_NAME=${!PARAM_PROFILE_NAME}
-REGION=${!!PARAM_REGION}
-REPO=${!PARAM_REPO}
-REPO_SCAN_ON_PUSH=${!PARAM_REPO_SCAN_ON_PUSH}
-PUBLIC_REGISTRY=${!PARAM_PUBLIC_REGISTRY}
-
-if [ "$PUBLIC_REGISTRY" == "1" ]; then
-    aws ecr-public describe-repositories --profile "${PROFILE_NAME}" --region us-east-1 --repository-names "${REPO}" > /dev/null 2>&1 || \
-    aws ecr-public create-repository --profile "${PROFILE_NAME}" --region us-east-1 --repository-name "${REPO}"
+if [ "$PARAM_PUBLIC_REGISTRY" == "1" ]; then
+    aws ecr-public describe-repositories --profile "${PARAM_PROFILE_NAME}" --region us-east-1 --repository-names "${PARAM_REPO}" > /dev/null 2>&1 || \
+    aws ecr-public create-repository --profile "${PARAM_PROFILE_NAME}" --region us-east-1 --repository-name "${PARAM_REPO}"
 else
-    aws ecr describe-repositories --profile "${PROFILE_NAME}" --region "${REGION}" --repository-names "${REPO}" > /dev/null 2>&1 || \
-    if [ "$REPO_SCAN_ON_PUSH" == "1" ]; then
-        aws ecr create-repository --profile "${PROFILE_NAME}" --region "${REGION}" --repository-name "${REPO}" --image-scanning-configuration scanOnPush=true
+    aws ecr describe-repositories --profile "${PARAM_PROFILE_NAME}" --region "${REGION}" --repository-names "${PARAM_REPO}" > /dev/null 2>&1 || \
+    if [ "$PARAM_REPO_SCAN_ON_PUSH" == "1" ]; then
+        aws ecr create-repository --profile "${PARAM_PROFILE_NAME}" --region "${REGION}" --repository-name "${PARAM_REPO}" --image-scanning-configuration scanOnPush=true
     else
-        aws ecr create-repository --profile "${PROFILE_NAME}" --region "${REGION}" --repository-name "${REPO}" --image-scanning-configuration scanOnPush=false
+        aws ecr create-repository --profile "${PARAM_PROFILE_NAME}" --region "${REGION}" --repository-name "${PARAM_REPO}" --image-scanning-configuration scanOnPush=false
     fi
 fi
 
--- src/scripts/docker-buildx.sh
+++ src/scripts/docker-buildx.sh
@@ -7,59 +7,47 @@
 # DOCKERFILE=$(eval echo "${PARAM_DOCKERFILE}")
 # PROFILE_NAME=$(eval echo "${PARAM_PROFILE_NAME}")
 # REGISTRY_ID=$(eval echo "\$${PARAM_REGISTRY_ID}")
-# REGION=$(eval echo "${PARAM_REGION}")
 # PLATFORM=$(eval echo "${PARAM_PLATFORM}")
 # PUBLIC_REGISTRY=$(eval echo "${PARAM_PUBLIC_REGISTRY}")
 # PUSH_IMAGE=$(eval echo "${PARAM_PUSH_IMAGE}")
 
-TAG=${!PARAM_TAG}
-SKIP_WHEN_TAGS_EXIST=${!PARAM_SKIP_WHEN_TAGS_EXIST}
-REPO=${!PARAM_REPO}
-EXTRA_BUILD_ARGS=${!PARAM_EXTRA_BUILD_ARGS}
-FILE_PATH=${!PARAM_PATH}
-DOCKERFILE=${!PARAM_DOCKERFILE}
-PROFILE_NAME=${!PARAM_PROFILE_NAME}
-REGISTRY_ID=${!PARAM_REGISTRY_ID}
-REGION=${!!AWS_REGION}
-PLATFORM=${!PARAM_PLATFORM}
-PUBLIC_REGISTRY=${!PARAM_PUBLIC_REGISTRY}
-PUSH_IMAGE=${!PARAM_PUSH_IMAGE}
+REGION=$(eval echo "${PARAM_REGION}")
 
-ACCOUNT_URL="${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com"
+ACCOUNT_URL="${PARAM_REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com"
 number_of_tags_in_ecr=0
 docker_tag_args=""
 ECR_COMMAND="ecr"
 
-if [ "$PUBLIC_REGISTRY" == "1" ]; then
+if [ "$PARAM_PUBLIC_REGISTRY" == "1" ]; then
     ECR_COMMAND="ecr-public"
-    ACCOUNT_URL="public.ecr.aws/${REGISTRY_ID}"
+    ACCOUNT_URL="public.ecr.aws/${PARAM_REGISTRY_ID}"
 fi
 
-IFS="," read -ra DOCKER_TAGS <<< "${TAG}"
-for tag in "${DOCKER_TAGS[@]}"; do
-  if [ "${SKIP_WHEN_TAGS_EXIST}" = "1" ]; then
-      docker_tag_exists_in_ecr=$(aws "${ECR_COMMAND}" describe-images --profile "${PROFILE_NAME}" --registry-id "${REGISTRY_ID}" --region "${REGION}" --repository-name "${REPO}" --query "contains(imageDetails[].imageTags[], '${tag}')")
+IFS="," read -ra DOCKER_TAGS <<< "${PARAM_TAG}"
+for tag in "${PARAM_DOCKER_TAGS[@]}"; do
+  if [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "1" ]; then
+      docker_tag_exists_in_ecr=$(aws "${ECR_COMMAND}" describe-images --profile "${PARAM_PROFILE_NAME}" --registry-id "${PARAM_REGISTRY_ID}" --region "${REGION}" --repository-name "${PARAM_REPO}" --query "contains(imageDetails[].imageTags[], '${tag}')")
     if [ "${docker_tag_exists_in_ecr}" = "1" ]; then
-      docker pull "${ACCOUNT_URL}/${REPO}:${tag}"
+      docker pull "${ACCOUNT_URL}/${PARAM_REPO}:${tag}"
       let "number_of_tags_in_ecr+=1"
     fi
   fi
-  docker_tag_args="${docker_tag_args} -t ${ACCOUNT_URL}/${REPO}:${tag}"
+  docker_tag_args="${docker_tag_args} -t ${ACCOUNT_URL}/${PARAM_REPO}:${tag}"
 done
 
-if [ "${SKIP_WHEN_TAGS_EXIST}" = "0" ] || [ "${SKIP_WHEN_TAGS_EXIST}" = "1" -a ${number_of_tags_in_ecr} -lt ${#DOCKER_TAGS[@]} ]; then
-    if [ "$PUSH_IMAGE" == "1" ]; then
+if [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "0" ] || [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "1" -a ${number_of_tags_in_ecr} -lt ${#DOCKER_TAGS[@]} ]; then
+    if [ "$PARAM_PUSH_IMAGE" == "1" ]; then
       set -- "$@" --push
     fi 
-    if [ -n "$EXTRA_BUILD_ARGS" ]; then
-       set -- "$@" "${EXTRA_BUILD_ARGS}"
+    if [ -n "$PARAM_EXTRA_BUILD_ARGS" ]; then
+       set -- "$@" "${PARAM_EXTRA_BUILD_ARGS}"
     fi
 
     docker buildx build \
-    -f "${FILE_PATH}"/"${DOCKERFILE}" \
+    -f "${PARAM_PATH}"/"${PARAM_DOCKERFILE}" \
     ${docker_tag_args} \
-    --platform "${PLATFORM}" \
+    --platform "${PARAM_PLATFORM}" \
     --progress plain \
     "$@" \
-    "${FILE_PATH}"     
+    "${PARAM_PATH}"     
 fi

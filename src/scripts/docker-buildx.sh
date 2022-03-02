a99ef468240be911130ae0b06772f17b837a4e36
Brian Vu <emailbrian.vu@gmail.com>
2022-03-02 01:28:05 -0800

    chore: code clean up

--- src/scripts/docker-buildx.sh
+++ src/scripts/docker-buildx.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-# TAG=$(eval echo "$PARAM_TAG")
-# SKIP_WHEN_TAGS_EXIST=$(eval echo "$PARAM_SKIP_WHEN_TAGS_EXIST")  
 REPO=$(eval echo "${PARAM_REPO}")
-# EXTRA_BUILD_ARGS=$(eval echo "${PARAM_EXTRA_BUILD_ARGS}")
-# FILE_PATH=$(eval echo "${PARAM_PATH}")
-# DOCKERFILE=$(eval echo "${PARAM_DOCKERFILE}")
-# PROFILE_NAME=$(eval echo "${PARAM_PROFILE_NAME}")
-# REGISTRY_ID=$(eval echo "\$${PARAM_REGISTRY_ID}")
 REGION=$(eval echo "${PARAM_REGION}")
-# PLATFORM=$(eval echo "${PARAM_PLATFORM}")
-PUBLIC_REGISTRY=$(eval echo "${PARAM_PUBLIC_REGISTRY}")
-# PUSH_IMAGE=$(eval echo "${PARAM_PUSH_IMAGE}")
-
 ACCOUNT_URL="${!PARAM_REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com"
+ECR_COMMAND="ecr"
 number_of_tags_in_ecr=0
 docker_tag_args=""
-ECR_COMMAND="ecr"
 
 if [ "$PARAM_PUBLIC_REGISTRY" == "1" ]; then
     ECR_COMMAND="ecr-public"
@@ -33,11 +22,12 @@ for tag in "${DOCKER_TAGS[@]}"; do
   fi
   docker_tag_args="${docker_tag_args} -t ${ACCOUNT_URL}/${REPO}:${tag}"
 done
-echo "tag arfs ${docker_tag_args}" >> test.txt
+
 if [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "0" ] || [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "1" -a ${number_of_tags_in_ecr} -lt ${#DOCKER_TAGS[@]} ]; then
     if [ "$PARAM_PUSH_IMAGE" == "1" ]; then
       set -- "$@" --push
-    fi 
+    fi
+
     if [ -n "$PARAM_EXTRA_BUILD_ARGS" ]; then
        set -- "$@" "${PARAM_EXTRA_BUILD_ARGS}"
     fi

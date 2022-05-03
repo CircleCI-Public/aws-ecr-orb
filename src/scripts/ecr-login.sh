20543408da67b0013c3adea8a0dac192ab0b5540
Brian Vu <64455338+brivu@users.noreply.github.com>
2022-05-03 11:13:02 -0700

    fix: v8.1.2 (#196)
    * fix: added validation for then parameter expands to true or 1
    
    * fix: added validation additional validation for AWS Credential Helper
    
    * fix: fixed conditional

--- src/scripts/ecr-login.sh
+++ src/scripts/ecr-login.sh
@@ -13,7 +13,8 @@ if [ -n "${PARAM_PROFILE_NAME}" ]; then
     set -- "$@" --profile "${PARAM_PROFILE_NAME}"
 fi
 
-if [ -f ~/.docker/config.json ]; then
+#Shellcheck disable=SC2002
+if cat ~/.docker/config.json | grep "${PARAM_ACCOUNT_URL}" > /dev/null 2>&1 ; then
     echo "Credential helper is already installed"
 else
     aws "${ECR_COMMAND}" get-login-password --region "${PARAM_REGION}" "$@" | docker login --username AWS --password-stdin "${PARAM_ACCOUNT_URL}"

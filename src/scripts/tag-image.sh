7ceb2edb57dc9f42a1ecc5ec3a5830fc00140ab8
Brian Vu <64455338+brivu@users.noreply.github.com>
2022-05-03 13:07:07 -0700

    fix: expanded target-tag parameter with eval (#198)

--- src/scripts/tag-image.sh
+++ src/scripts/tag-image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PARAM_REPO=$(eval echo "${PARAM_REPO}")
 PARAM_SOURCE_TAG=$(eval echo "${PARAM_SOURCE_TAG}")
+PARAM_TARGET_TAG=$(eval echo "${PARAM_TARGET_TAG}")
 
 # pull the image manifest from ECR
 MANIFEST=$(aws ecr batch-get-image --repository-name "${PARAM_REPO}" --image-ids imageTag="${PARAM_SOURCE_TAG}" --query 'images[].imageManifest' --output text)

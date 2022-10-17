86e04a63918154fd3f04c620104d3025ad3b22ee
Brian Vu <emailbrian.vu@gmail.com>
2022-10-17 11:45:35 -0700

    test: debugging

--- src/scripts/docker-buildx.sh
+++ src/scripts/docker-buildx.sh
@@ -60,10 +60,11 @@ if [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "0" ] || [[ "${PARAM_SKIP_WHEN_TAGS_EXIST
       "$@" \
       "${PARAM_PATH}"
   else
+
     if ! docker context ls | grep builder; then
       # We need to skip the creation of the builder context if it's already present
       # otherwise the command will fail when called more than once in the same job.
-
+      echo "debug 1"
       docker context create builder
       docker run --privileged --rm tonistiigi/binfmt --install all
       docker --context builder buildx create --use

a83d5142574ab57a7e38eaacf5552b0e7bee3334
Brian Vu <emailbrian.vu@gmail.com>
2022-10-21 13:21:44 -0700

    fix: resolved pr comments

--- src/scripts/create-repo.sh
+++ src/scripts/create-repo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-PARAM_REGION=$(eval echo "${PARAM_REGION}")
-PARAM_REPO=$(eval echo "${PARAM_REPO}")
+ORB_EVAL_REGION=$(eval echo "${ORB_EVAL_REGION}")
+ORB_EVAL_REPO=$(eval echo "${ORB_EVAL_REPO}")
 
-if [ "$PARAM_PUBLIC_REGISTRY" == "1" ]; then
-    aws ecr-public describe-repositories --profile "${PARAM_PROFILE_NAME}" --region us-east-1 --repository-names "${PARAM_REPO}" >/dev/null 2>&1 ||
-        aws ecr-public create-repository --profile "${PARAM_PROFILE_NAME}" --region us-east-1 --repository-name "${PARAM_REPO}"
+if [ "$ORB_EVAL_PUBLIC_REGISTRY" == "1" ]; then
+    aws ecr-public describe-repositories --profile "${ORB_EVAL_PROFILE_NAME}" --region us-east-1 --repository-names "${ORB_EVAL_REPO}" >/dev/null 2>&1 ||
+        aws ecr-public create-repository --profile "${ORB_EVAL_PROFILE_NAME}" --region us-east-1 --repository-name "${ORB_EVAL_REPO}"
 else
-    aws ecr describe-repositories --profile "${PARAM_PROFILE_NAME}" --region "${PARAM_REGION}" --repository-names "${PARAM_REPO}" >/dev/null 2>&1 ||
-        if [ "$PARAM_REPO_SCAN_ON_PUSH" == "1" ]; then
-            aws ecr create-repository --profile "${PARAM_PROFILE_NAME}" --region "${PARAM_REGION}" --repository-name "${PARAM_REPO}" --image-scanning-configuration scanOnPush=true
+    aws ecr describe-repositories --profile "${ORB_EVAL_PROFILE_NAME}" --region "${ORB_EVAL_REGION}" --repository-names "${ORB_EVAL_REPO}" >/dev/null 2>&1 ||
+        if [ "$ORB_EVAL_REPO_SCAN_ON_PUSH" == "1" ]; then
+            aws ecr create-repository --profile "${ORB_EVAL_PROFILE_NAME}" --region "${ORB_EVAL_REGION}" --repository-name "${ORB_EVAL_REPO}" --image-scanning-configuration scanOnPush=true
         else
-            aws ecr create-repository --profile "${PARAM_PROFILE_NAME}" --region "${PARAM_REGION}" --repository-name "${PARAM_REPO}" --image-scanning-configuration scanOnPush=false
+            aws ecr create-repository --profile "${ORB_EVAL_PROFILE_NAME}" --region "${ORB_EVAL_REGION}" --repository-name "${ORB_EVAL_REPO}" --image-scanning-configuration scanOnPush=false
         fi
 fi
--- src/scripts/docker-buildx.sh
+++ src/scripts/docker-buildx.sh
@@ -1,66 +1,66 @@
 #!/bin/bash
-PARAM_REGION=$(eval echo "${PARAM_REGION}")
-PARAM_REPO=$(eval echo "${PARAM_REPO}")
-PARAM_TAG=$(eval echo "${PARAM_TAG}")
-PARAM_PATH=$(eval echo "${PARAM_PATH}")
-PARAM_ACCOUNT_URL="${!PARAM_REGISTRY_ID}.dkr.ecr.${PARAM_REGION}.amazonaws.com"
-PARAM_PUBLIC_REGISTRY_ALIAS=$(eval echo "${PARAM_PUBLIC_REGISTRY_ALIAS}")
+ORB_EVAL_REGION=$(eval echo "${ORB_EVAL_REGION}")
+ORB_EVAL_REPO=$(eval echo "${ORB_EVAL_REPO}")
+ORB_EVAL_TAG=$(eval echo "${ORB_EVAL_TAG}")
+ORB_EVAL_PATH=$(eval echo "${ORB_EVAL_PATH}")
+ORB_EVAL_ACCOUNT_URL="${!ORB_EVAL_REGISTRY_ID}.dkr.ecr.${ORB_EVAL_REGION}.amazonaws.com"
+ORB_EVAL_PUBLIC_REGISTRY_ALIAS=$(eval echo "${ORB_EVAL_PUBLIC_REGISTRY_ALIAS}")
 ECR_COMMAND="ecr"
 number_of_tags_in_ecr=0
 docker_tag_args=""
 
-IFS="," read -ra PLATFORMS <<<"${PARAM_PLATFORM}"
+IFS="," read -ra PLATFORMS <<<"${ORB_EVAL_PLATFORM}"
 arch_count=${#PLATFORMS[@]}
 
-if [ "${PARAM_PUBLIC_REGISTRY}" == "1" ]; then
+if [ "${ORB_EVAL_PUBLIC_REGISTRY}" == "1" ]; then
   if [ "$arch_count" -gt 1 ]; then
     echo "AWS ECR does not support multiple platforms for public registries. Please specify only one platform and try again"
     exit 1
   fi
 
   ECR_COMMAND="ecr-public"
-  PARAM_ACCOUNT_URL="public.ecr.aws/${PARAM_PUBLIC_REGISTRY_ALIAS}"
+  ORB_EVAL_ACCOUNT_URL="public.ecr.aws/${ORB_EVAL_PUBLIC_REGISTRY_ALIAS}"
 fi
 
-IFS="," read -ra DOCKER_TAGS <<<"${PARAM_TAG}"
+IFS="," read -ra DOCKER_TAGS <<<"${ORB_EVAL_TAG}"
 for tag in "${DOCKER_TAGS[@]}"; do
-  if [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "1" ] || [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "true" ]; then
-    docker_tag_exists_in_ecr=$(aws "${ECR_COMMAND}" describe-images --profile "${PARAM_PROFILE_NAME}" --registry-id "${!PARAM_REGISTRY_ID}" --region "${PARAM_REGION}" --repository-name "${PARAM_REPO}" --query "contains(imageDetails[].imageTags[], '${tag}')")
+  if [ "${ORB_EVAL_SKIP_WHEN_TAGS_EXIST}" = "1" ] || [ "${ORB_EVAL_SKIP_WHEN_TAGS_EXIST}" = "true" ]; then
+    docker_tag_exists_in_ecr=$(aws "${ECR_COMMAND}" describe-images --profile "${ORB_EVAL_PROFILE_NAME}" --registry-id "${!ORB_EVAL_REGISTRY_ID}" --region "${ORB_EVAL_REGION}" --repository-name "${ORB_EVAL_REPO}" --query "contains(imageDetails[].imageTags[], '${tag}')")
     if [ "${docker_tag_exists_in_ecr}" = "true" ]; then
-      docker pull "${PARAM_ACCOUNT_URL}/${PARAM_REPO}:${tag}"
+      docker pull "${ORB_EVAL_ACCOUNT_URL}/${ORB_EVAL_REPO}:${tag}"
       number_of_tags_in_ecr=$((number_of_tags_in_ecr += 1))
     fi
   fi
-  docker_tag_args="${docker_tag_args} -t ${PARAM_ACCOUNT_URL}/${PARAM_REPO}:${tag}"
+  docker_tag_args="${docker_tag_args} -t ${ORB_EVAL_ACCOUNT_URL}/${ORB_EVAL_REPO}:${tag}"
 done
 
-if [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "0" ] || [[ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "1" && ${number_of_tags_in_ecr} -lt ${#DOCKER_TAGS[@]} ]]; then
-  if [ "${PARAM_PUSH_IMAGE}" == "1" ]; then
+if [ "${ORB_EVAL_SKIP_WHEN_TAGS_EXIST}" = "0" ] || [[ "${ORB_EVAL_SKIP_WHEN_TAGS_EXIST}" = "1" && ${number_of_tags_in_ecr} -lt ${#DOCKER_TAGS[@]} ]]; then
+  if [ "${ORB_EVAL_PUSH_IMAGE}" == "1" ]; then
     set -- "$@" --push
 
-    if [ -n "${PARAM_LIFECYCLE_POLICY_PATH}" ]; then
+    if [ -n "${ORB_EVAL_LIFECYCLE_POLICY_PATH}" ]; then
       aws ecr put-lifecycle-policy \
-        --repository-name "${PARAM_REPO}" \
-        --lifecycle-policy-text "file://${PARAM_LIFECYCLE_POLICY_PATH}"
+        --repository-name "${ORB_EVAL_REPO}" \
+        --lifecycle-policy-text "file://${ORB_EVAL_LIFECYCLE_POLICY_PATH}"
     fi
 
   else
     set -- "$@" --load
   fi
 
-  if [ -n "$PARAM_EXTRA_BUILD_ARGS" ]; then
-    PARAM_EXTRA_BUILD_ARGS=$(eval echo "${PARAM_EXTRA_BUILD_ARGS}")
-    set -- "$@" ${PARAM_EXTRA_BUILD_ARGS}
+  if [ -n "$ORB_EVAL_EXTRA_BUILD_ARGS" ]; then
+    ORB_EVAL_EXTRA_BUILD_ARGS=$(eval echo "${ORB_EVAL_EXTRA_BUILD_ARGS}")
+    set -- "$@" ${ORB_EVAL_EXTRA_BUILD_ARGS}
   fi
 
-  if [ "${PARAM_PUBLIC_REGISTRY}" == "1" ]; then
+  if [ "${ORB_EVAL_PUBLIC_REGISTRY}" == "1" ]; then
     docker buildx build \
-      -f "${PARAM_PATH}"/"${PARAM_DOCKERFILE}" \
+      -f "${ORB_EVAL_PATH}"/"${ORB_EVAL_DOCKERFILE}" \
       ${docker_tag_args} \
-      --platform "${PARAM_PLATFORM}" \
+      --platform "${ORB_EVAL_PLATFORM}" \
       --progress plain \
       "$@" \
-      "${PARAM_PATH}"
+      "${ORB_EVAL_PATH}"
   else
 
     if ! docker context ls | grep builder; then
@@ -73,11 +73,11 @@ if [ "${PARAM_SKIP_WHEN_TAGS_EXIST}" = "0" ] || [[ "${PARAM_SKIP_WHEN_TAGS_EXIST
     fi
 
     docker --context builder buildx build \
-      -f "${PARAM_PATH}"/"${PARAM_DOCKERFILE}" \
+      -f "${ORB_EVAL_PATH}"/"${ORB_EVAL_DOCKERFILE}" \
       ${docker_tag_args} \
-      --platform "${PARAM_PLATFORM}" \
+      --platform "${ORB_EVAL_PLATFORM}" \
       --progress plain \
       "$@" \
-      "${PARAM_PATH}"
+      "${ORB_EVAL_PATH}"
   fi
 fi

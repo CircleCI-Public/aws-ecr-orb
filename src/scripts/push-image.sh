7dd33c9fcc6387a04e503bbd03a311fa9fe6d910
Brian Vu <emailbrian.vu@gmail.com>
2022-11-03 11:33:19 -0700

    feat: added public registry support

--- src/scripts/push-image.sh
+++ src/scripts/push-image.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-ORB_VAL_ACCOUNT_URL=$(eval echo "${ORB_ENV_REGISTRY_ID}.dkr.ecr.${ORB_EVAL_REGION}.amazonaws.com")
 ORB_EVAL_REPO=$(eval echo "${ORB_EVAL_REPO}")
 ORB_EVAL_TAG=$(eval echo "${ORB_EVAL_TAG}")
+ORB_EVAL_REGION=$(eval echo "${ORB_EVAL_REGION}")
+ORB_VAL_ACCOUNT_URL="${!ORB_ENV_REGISTRY_ID}.dkr.ecr.${ORB_EVAL_REGION}.amazonaws.com"
+ORB_EVAL_PUBLIC_REGISTRY_ALIAS=$(eval echo "${ORB_EVAL_PUBLIC_REGISTRY_ALIAS}")
+
+if [ "${ORB_VAL_PUBLIC_REGISTRY}" == "1" ]; then
+  ORB_VAL_ACCOUNT_URL="public.ecr.aws/${ORB_EVAL_PUBLIC_REGISTRY_ALIAS}"
+fi
 
 IFS="," read -ra DOCKER_TAGS <<< "${ORB_EVAL_TAG}"
 for tag in "${DOCKER_TAGS[@]}"; do

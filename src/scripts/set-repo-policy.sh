0e9e008844468244bb4308f2cd51ead58269e402
tweeks-reify <tom.weeks@reifyhealth.com>
2022-11-08 09:48:49 -0500

    feat: Add set-repository-permissions

---
+++ src/scripts/set-repo-policy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+ORB_EVAL_REGION=$(eval echo "${ORB_EVAL_REGION}")
+ORB_EVAL_REPO=$(eval echo "${ORB_EVAL_REPO}")
+
+if [ "$ORB_VAL_PUBLIC_REGISTRY" == "1" ]; then
+    echo "set-repository-policy is not supported on public repos"
+    exit 1
+else
+    aws ecr set-repository-policy \
+        --profile "${ORB_VAL_PROFILE_NAME}" \
+        --region "${ORB_EVAL_REGION}" \
+        --repository-name "${ORB_EVAL_REPO}" \
+        --policy-text "file://${ORB_VAL_REPO_POLICY_PATH}"
+fi

e9e1614d1010d8a65589907d56640fba29fa8dcd
Brian Vu <emailbrian.vu@gmail.com>
2022-11-16 00:11:28 -0800

    feat: add validation for non-existent registry id

--- src/scripts/push-image.sh
+++ src/scripts/push-image.sh
@@ -5,6 +5,11 @@ ORB_EVAL_REGION=$(eval echo "${ORB_EVAL_REGION}")
 ORB_VAL_ACCOUNT_URL="${!ORB_ENV_REGISTRY_ID}.dkr.ecr.${ORB_EVAL_REGION}.amazonaws.com"
 ORB_EVAL_PUBLIC_REGISTRY_ALIAS=$(eval echo "${ORB_EVAL_PUBLIC_REGISTRY_ALIAS}")
 
+if [ -z "${!ORB_ENV_REGISTRY_ID}" ]; then
+  echo "The registry ID is not found. Please add the registry ID as an environment variable in CicleCI before continuing."
+  exit 1
+fi
+
 if [ "${ORB_VAL_PUBLIC_REGISTRY}" == "1" ]; then
   ORB_VAL_ACCOUNT_URL="public.ecr.aws/${ORB_EVAL_PUBLIC_REGISTRY_ALIAS}"
 fi

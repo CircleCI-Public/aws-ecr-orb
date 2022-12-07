4a070ce4c597e70fa75dcd2371936a895a030f06
Brian Vu <emailbrian.vu@gmail.com>
2022-12-07 00:31:26 -0800

    test: unquote context args

--- src/scripts/docker-buildx.sh
+++ src/scripts/docker-buildx.sh
@@ -55,13 +55,13 @@ if [ "${ORB_VAL_SKIP_WHEN_TAGS_EXIST}" = "0" ] || [[ "${ORB_VAL_SKIP_WHEN_TAGS_E
     docker context create builder
     docker run --privileged --rm tonistiigi/binfmt --install all
     docker --context builder buildx create --use
-    # context_args=--context builder
+    context_args="--context builder"
   fi
   # DOCKER_COMMAND=$(eval echo "docker ${DOCKER_CONTEXT} buildx build")
-    # docker \
-    # buildx build \
-    # ${context_args:+"$context_args"} \
-  docker --context builder buildx build \
+  # docker \ --context builder buildx build \
+    docker \
+    buildx build \
+    ${context_args} \
     -f "${ORB_EVAL_PATH}"/"${ORB_VAL_DOCKERFILE}" \
     $docker_tag_args \
     --platform "${ORB_VAL_PLATFORM}" \

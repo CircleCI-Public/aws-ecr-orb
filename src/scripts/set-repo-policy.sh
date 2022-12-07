51202d0dc83f14007e5de7467b801a1d5f9053de
Brian Vu <emailbrian.vu@gmail.com>
2022-12-07 15:19:31 -0800

    fix: add valid sample repo-policy

--- src/scripts/set-repo-policy.sh
+++ src/scripts/set-repo-policy.sh
@@ -6,9 +6,11 @@ if [ "$ORB_VAL_PUBLIC_REGISTRY" == "1" ]; then
     echo "set-repository-policy is not supported on public repos"
     exit 1
 else
+    set -x
     aws ecr set-repository-policy \
         --profile "${ORB_VAL_PROFILE_NAME}" \
         --region "${ORB_EVAL_REGION}" \
         --repository-name "${ORB_EVAL_REPO}" \
         --policy-text "file://${ORB_VAL_REPO_POLICY_PATH}"
+    set +x
 fi

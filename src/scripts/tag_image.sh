54573c8c1bd8a612c9afc4ca96f0423c2adf84d7
Brian Vu <emailbrian.vu@gmail.com>
2023-06-14 22:04:40 -0700

    fix: resolve pr comments

--- src/scripts/tag_image.sh
+++ src/scripts/tag_image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ORB_STR_REPO="$(circleci env subst "${ORB_STR_REPO}")"
-ORB_EVAL_SOURCE_TAG="$(circleci env subst "${ORB_EVAL_SOURCE_TAG}")"
-ORB_EVAL_TARGET_TAG="$(circleci env subst "${ORB_EVAL_TARGET_TAG}")"
+ORB_STR_SOURCE_TAG="$(circleci env subst "${ORB_STR_SOURCE_TAG}")"
+ORB_STR_TARGET_TAG="$(circleci env subst "${ORB_STR_TARGET_TAG}")"
 
 # pull the image manifest from ECR
-MANIFEST=$(aws ecr batch-get-image --repository-name "${ORB_STR_REPO}" --image-ids imageTag="${ORB_EVAL_SOURCE_TAG}" --query 'images[].imageManifest' --output text)
-IFS="," read -ra ECR_TAGS <<<"${ORB_EVAL_TARGET_TAG}"
+MANIFEST=$(aws ecr batch-get-image --repository-name "${ORB_STR_REPO}" --image-ids imageTag="${ORB_STR_SOURCE_TAG}" --query 'images[].imageManifest' --output text)
+IFS="," read -ra ECR_TAGS <<<"${ORB_STR_TARGET_TAG}"
 for tag in "${ECR_TAGS[@]}"; do
     aws ecr put-image --repository-name "${ORB_STR_REPO}" --image-tag "${tag}" --image-manifest "${MANIFEST}"
 done

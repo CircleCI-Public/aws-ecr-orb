ff3f0cd6e7b71f7fb9738d7a8ede951f43c591c0
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-06-15 10:28:51 -0700

    chore: update src/scripts/tag_image.sh
    Co-authored-by: Eric Ribeiro <ericribeiro@outlook.com.br>

--- src/scripts/tag_image.sh
+++ src/scripts/tag_image.sh
@@ -4,7 +4,7 @@ ORB_STR_SOURCE_TAG="$(circleci env subst "${ORB_STR_SOURCE_TAG}")"
 ORB_STR_TARGET_TAG="$(circleci env subst "${ORB_STR_TARGET_TAG}")"
 
 # pull the image manifest from ECR
-MANIFEST=$(aws ecr batch-get-image --repository-name "${ORB_STR_REPO}" --image-ids imageTag="${ORB_STR_SOURCE_TAG}" --query 'images[].imageManifest' --output text)
+MANIFEST="$(aws ecr batch-get-image --repository-name "${ORB_STR_REPO}" --image-ids imageTag="${ORB_STR_SOURCE_TAG}" --query 'images[].imageManifest' --output text)"
 IFS="," read -ra ECR_TAGS <<<"${ORB_STR_TARGET_TAG}"
 for tag in "${ECR_TAGS[@]}"; do
     aws ecr put-image --repository-name "${ORB_STR_REPO}" --image-tag "${tag}" --image-manifest "${MANIFEST}"

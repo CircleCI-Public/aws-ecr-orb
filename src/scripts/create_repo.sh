fb91802997a779dc9d2ac5a6e4db71520ac22c11
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-07-26 18:09:25 -0400

    feat: repository encryption parameter (#289)
    * feat: Adds params to control repo encryption during repo creation (#284)
    
    * chore: code cleanup
    
    * fix: extra "<" in parameters (#287)
    
    * feat: Adds params to control repo encryption during repo creation
    
    * fix: extra "<" in parameters
    
    ---------
    
    Co-authored-by: Brian Vu <64455338+brivu@users.noreply.github.com>
    
    * chore: update aws_domain description
    
    * chore: update aws_domain description
    
    * refactor: add env subst
    
    * fix: remove duplicate expansion
    
    * fix: address pr issues
    
    * chore: rename parameters
    
    ---------
    
    Co-authored-by: Ilya Sabelnikov <fruit@users.noreply.github.com>

--- src/scripts/create_repo.sh
+++ src/scripts/create_repo.sh
@@ -2,15 +2,31 @@
 ORB_STR_REGION="$(circleci env subst "${ORB_STR_REGION}")"
 ORB_STR_REPO="$(circleci env subst "${ORB_STR_REPO}")"
 ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
+ORB_STR_ENCRYPTION_KMS_KEY="$(circleci env subst "${ORB_STR_ENCRYPTION_KMS_KEY}")"
 
 if [ "$ORB_BOOL_PUBLIC_REGISTRY" == "1" ]; then
     aws ecr-public describe-repositories --profile "${ORB_STR_PROFILE_NAME}" --region us-east-1 --repository-names "${ORB_STR_REPO}" >/dev/null 2>&1 ||
         aws ecr-public create-repository --profile "${ORB_STR_PROFILE_NAME}" --region us-east-1 --repository-name "${ORB_STR_REPO}"
 else
-    aws ecr describe-repositories --profile "${ORB_STR_PROFILE_NAME}" --region "${ORB_STR_REGION}" --repository-names "${ORB_STR_REPO}" >/dev/null 2>&1 ||
-        if [ "$ORB_BOOL_REPO_SCAN_ON_PUSH" == "1" ]; then
-            aws ecr create-repository --profile "${ORB_STR_PROFILE_NAME}" --region "${ORB_STR_REGION}" --repository-name "${ORB_STR_REPO}" --image-scanning-configuration scanOnPush=true
-        else
-            aws ecr create-repository --profile "${ORB_STR_PROFILE_NAME}" --region "${ORB_STR_REGION}" --repository-name "${ORB_STR_REPO}" --image-scanning-configuration scanOnPush=false
-        fi
+
+    IMAGE_SCANNING_CONFIGURATION="scanOnPush=true"
+    if [ "$ORB_BOOL_REPO_SCAN_ON_PUSH" -ne "1" ]; then
+      IMAGE_SCANNING_CONFIGURATION="scanOnPush=false"
+    fi
+
+    ENCRYPTION_CONFIGURATION="encryptionType=${ORB_ENUM_ENCRYPTION_TYPE}"
+    if [ "$ORB_ENUM_ENCRYPTION_TYPE" == "KMS"  ]; then
+      ENCRYPTION_CONFIGURATION+=",kmsKey=${ORB_STR_ENCRYPTION_KMS_KEY}"
+    fi
+
+    aws ecr describe-repositories \
+      --profile "${ORB_STR_PROFILE_NAME}" \
+      --region "${ORB_STR_REGION}" \
+      --repository-names "${ORB_STR_REPO}" >/dev/null 2>&1 ||
+        aws ecr create-repository \
+          --profile "${ORB_STR_PROFILE_NAME}" \
+          --region "${ORB_STR_REGION}" \
+          --repository-name "${ORB_STR_REPO}" \
+          --image-scanning-configuration "${IMAGE_SCANNING_CONFIGURATION}" \
+          --encryption-configuration "${ENCRYPTION_CONFIGURATION}"
 fi

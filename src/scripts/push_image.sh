3c1478d9968660ea00dd927db8eed75eac9df769
Brian Vu <emailbrian.vu@gmail.com>
2024-01-18 11:33:20 -0800

    refactor: trim CIRCLE_SHA1

--- src/scripts/push_image.sh
+++ src/scripts/push_image.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -x
 ORB_EVAL_REPO="$(eval echo "${ORB_STR_REPO}")"
 ORB_EVAL_TAG="$(eval echo "${ORB_STR_TAG}")"
 ORB_EVAL_REGION="$(eval echo "${ORB_STR_REGION}")"
 ORB_EVAL_ACCOUNT_ID="$(eval echo "${ORB_STR_ACCOUNT_ID}")"
-ORB_VAL_ACCOUNT_URL="${ORB_EVAL_ACCOUNT_ID}.dkr.ecr.${ORB_EVAL_REGION}.${ORB_EVAL_AWS_DOMAIN}"
+ORB_VAL_ACCOUNT_URL="${ORB_EVAL_ACCOUNT_ID}.dkr.ecr.${ORB_EVAL_REGION}"
 ORB_EVAL_PUBLIC_REGISTRY_ALIAS="$(eval echo "${ORB_STR_PUBLIC_REGISTRY_ALIAS}")"
+
+echo "$ORB_VAL_ACCOUNT_URL" >> test.txt
 
 if [ -z "${ORB_EVAL_ACCOUNT_ID}" ]; then
   echo "The account ID is not found. Please add the account ID before continuing."
@@ -19,3 +22,6 @@ IFS="," read -ra DOCKER_TAGS <<< "${ORB_EVAL_TAG}"
 for tag in "${DOCKER_TAGS[@]}"; do
     docker push "${ORB_VAL_ACCOUNT_URL}/${ORB_EVAL_REPO}:${tag}"
 done
+
+docker image ls >> test.txt
+set +x

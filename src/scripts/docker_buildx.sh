5491fece4b60896678c80e0a9d0898ab89765565
Brian Vu <64455338+brivu@users.noreply.github.com>
2024-01-22 17:37:53 -0800

    fix: switch buildx command to fix validation (#323)
    * fix: switch buildx command to fix validation
    
    * chore: add debug and context confirmation

--- src/scripts/docker_buildx.sh
+++ src/scripts/docker_buildx.sh
@@ -76,8 +76,11 @@ if [ "${AWS_ECR_BOOL_SKIP_WHEN_TAGS_EXIST}" -eq "0" ] || [[ "${AWS_ECR_BOOL_SKIP
     fi
     context_args="--context builder"
   # if no builder instance is currently used, create one
-  elif ! docker buildx inspect | grep -q "default * docker"; then
+  elif ! docker buildx ls | grep -q "default * docker"; then
+    set -x
     docker buildx create --name DLC_builder --use
+    echo "Context is set to DLC_builder"
+    set +x
   fi
 
 set -x

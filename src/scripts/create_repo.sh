e216d8f5f69d5b24af2341e5bc32d552948a92c2
Ethan Miller <ethan.miller@expel.io>
2025-11-25 10:17:19 -0500

    Add support for repo image tag mutability: IMMUTABLE_WITH_EXCLUSION

--- src/scripts/create_repo.sh
+++ src/scripts/create_repo.sh
@@ -4,6 +4,7 @@ AWS_ECR_EVAL_REPO="$(eval echo "${AWS_ECR_STR_REPO}")"
 AWS_ECR_EVAL_PROFILE_NAME="$(eval echo "${AWS_ECR_STR_PROFILE_NAME}")"
 AWS_ECR_EVAL_ENCRYPTION_KMS_KEY="$(eval echo "${AWS_ECR_STR_ENCRYPTION_KMS_KEY}")"
 AWS_ECR_EVAL_IMAGE_TAG_MUTABILITY="$(eval echo "${AWS_ECR_STR_IMAGE_TAG_MUTABILITY}")"
+AWS_ECR_EVAL_IMAGE_TAG_EXCLUSION_FILTERS="$(eval echo "${AWS_ECR_STR_IMAGE_TAG_EXCLUSION_FILTERS}")"
 AWS_ECR_EVAL_REPO_TAG="$(eval echo "${AWS_ECR_STR_REPO_TAG}")"
 
 if [ "$AWS_ECR_BOOL_PUBLIC_REGISTRY" == "1" ]; then
@@ -25,6 +26,15 @@ else
         set -- "$@" --tags
         set -- "$@" "${AWS_ECR_EVAL_REPO_TAG}"
     fi
+
+    if [ "${AWS_ECR_EVAL_IMAGE_TAG_MUTABILITY}" == "IMMUTABLE_WITH_EXCLUSION" ] && [ -n "${AWS_ECR_EVAL_IMAGE_TAG_EXCLUSION_FILTERS}" ]; then
+      IFS=',' read -ra FILTERS <<< "${AWS_ECR_EVAL_IMAGE_TAG_EXCLUSION_FILTERS}"
+      for filter in "${FILTERS[@]}"; do
+        set -- "$@" --image-tag-mutability-exclusion-filters
+        set -- "$@" "filterType=WILDCARD,filter=${filter}"
+      done
+    fi
+    
     aws ecr describe-repositories \
       --profile "${AWS_ECR_EVAL_PROFILE_NAME}" \
       --region "${AWS_ECR_EVAL_REGION}" \
